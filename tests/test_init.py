fd4c321a523edf3d5fbaa598687a3cf26ca09012
JimmyTournemaine <jimdeplouz@gmail.com>
2024-02-12 23:21:12 +0000

    Fixes

--- tests/test_init.py
+++ tests/test_init.py
@@ -14,10 +14,10 @@ async def test_unload_entry(
     await hass.config_entries.async_setup(mock_config_entry.entry_id)
     await hass.async_block_till_done()
 
-    assert mock_config_entry.state is ConfigEntryState.LOADED # type: ignore[comparison-overlap]
+    assert mock_config_entry.state is ConfigEntryState.LOADED
 
     await hass.config_entries.async_unload(mock_config_entry.entry_id)
     await hass.async_block_till_done()
 
-    assert mock_config_entry.state is ConfigEntryState.NOT_LOADED # type: ignore[comparison-overlap]
+    assert mock_config_entry.state is ConfigEntryState.NOT_LOADED  # type: ignore[comparison-overlap]
     assert not hass.data.get(DOMAIN)
